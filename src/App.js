78d63bf6c78a89ad92d4160433ae4c44ccdb4053
Nurettin Demirhan <ndemirhan1907@gmail.com>
2023-04-27 23:27:49 +0300

    added the hero section component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import FanFavourites from './components/fan-favourites/FanFavourites';
+import HeroSection from './components/hero-section/HeroSection';
 
 function App() {
   return (
     <div className="App">
+      <HeroSection/>
       <FanFavourites/>
       <Footer/>
     </div>
