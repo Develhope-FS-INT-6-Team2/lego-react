00f3bd42e1c6678a38dd967f1fd92a321a5e62e5
Nurettin Demirhan <ndemirhan1907@gmail.com>
2023-04-27 19:38:38 +0300

    added the fan favourites section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer/Footer';
+import FanFavourites from './components/fan-favourites/FanFavourites';
 
 function App() {
   return (
     <div className="App">
-
+      <FanFavourites/>
       <Footer/>
     </div>
   );
