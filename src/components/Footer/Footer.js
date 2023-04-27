f5d3abc00cce545413d6c25dc0db1d5aa03b484d
Emre <ct.edag@gmail.com>
2023-04-27 22:25:07 +0300

    test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,9 @@
-import React from 'react';
-import './Footer.scss';
-
-const Footer = () => {
-  return (
-    <footer>
-      <div className="container">
-        <p>&copy; 2023 Lego. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-};
 import React from "react";
 import FooterMain from "./footer-main/FooterMain";
 import FooterEmailFollow from "./footer-email-follow/FooterEmailFollow";
 import FooterLinks from "./footer-links/FooterLinks";
 import "./footer.css";
+
 
 function Footer(){
     return(
