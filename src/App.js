3f395fd0306f40505729798e324e65752e84daf5
Nurettin Demirhan <ndemirhan1907@gmail.com>
2023-04-29 13:43:46 +0300

    added the BannerTwo component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Footer from './components/Footer/Footer';
 import FanFavourites from './components/fan-favourites/FanFavourites';
 import HeroSection from './components/hero-section/HeroSection';
 import CategoryNavbar from './components/category-navbar/CategoryNavbar';
+import BannerTwo from "./components/banner-2/BannerTwo"
 
 function App() {
   return (
     <div className="App">
       <HeroSection/>
       <CategoryNavbar/>
+      <BannerTwo/>
       <FanFavourites/>
       <Footer/>
     </div>
