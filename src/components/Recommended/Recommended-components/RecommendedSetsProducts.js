20ebba59fc2b09cc11be8429fa8d3478986d973f
AlbertoNeira <124807146+AlbertoNeira@users.noreply.github.com>
2023-05-08 13:45:13 +0200

    added some keys to products

--- src/components/Recommended/Recommended-components/RecommendedSetsProducts.js
+++ src/components/Recommended/Recommended-components/RecommendedSetsProducts.js
@@ -6,63 +6,84 @@ let sets =[
         "image": "../assets/images/recommended/emperor-throne.png",
         "title": "Emperor's Throne Room™ Diorama",
         "price": "89.99$",
-        "category": "Movies"
+        "category": "Movies",
+        "id":13,
+        "pieces":807,
     },
     {
         "image": "../assets/images/recommended/endor-speeder.png",
         "title": "Endor™ Speeder Chase Diorama",
         "price": "69.99$",
-        "category": "Movies"
+        "category": "Movies",
+        "id":14,
+        "pieces":763,
     },
     {
         "image": "../assets/images/recommended/mandalorian-fang-figther.png",
         "title": "Mandalorian Fighter vs. TIE™",
         "price": "89.99$",
-        "category": "Movies"
+        "category": "Movies",
+        "id":15,
+        "pieces":957,
+   
     },
     {
         "image": "../assets/images/recommended/starfighter.png",
         "title": "X-Wing Starfighter™",
         "price": "209.99$",
-        "category": "Movies"
+        "category": "Movies",
+        "id":16,
+        "pieces":1949,
     },
     {
         "image": "../assets/images/recommended/spider-tank.png",
         "title": "Spider Tank",
         "price": "39.99$",
-        "category": "Movies"
+        "category": "Movies",
+        "id":17,
+        "pieces":526,
     },
     {
         "image": "../assets/images/recommended/battle-endor-heros.png",
         "title": "Battle of Endor™ Heroes",
         "price": "39.99$",
-        "category": "Movies"
+        "category": "Movies",
+        "id":18,
+        "pieces":549,
     },
  
     {
         "image": "../assets/images/recommended/vader-keyring.png",
         "title": "Darth Vader™ Keyring",
         "price": "4.99$",
-        "category": "Keyring"
+        "category": "Keyring",
+        "id":19,
+        "pieces":1,
     },
   
     {
         "image": "../assets/images/recommended/mermaid.png",
         "title": "The Little Mermaid Royal Clamshell",
         "price": "139.99$",
-        "category": "toys"
+        "category": "toys",
+        "id":20,
+        "pieces":624,
     },
     {
         "image": "../assets/images//recommended/peugeot.png",
         "title": "PEUGEOT 9X8 Hybrid Hypercar",
         "price": "7169.99$",
-        "category": "Car"
+        "category": "Car",
+        "id":21,
+        "pieces":726,
     },
     {
         "image": "../assets/images/recommended/land-rover.png",
         "title": "Land Rover Classic Defender 90",
         "price": "209.99$",
-        "category": "Car"
+        "category": "Car",
+        "id":22,
+        "pieces":631,
     },
   
   
@@ -70,14 +91,18 @@ let sets =[
         "image": "../assets/images/recommended/tales-space.png",
         "title": "Tales of the Space Age",
         "price": "59.99$",
-        "category": "Space"
+        "category": "Space",
+        "id":23,
+        "pieces":688,
     },
   
     {
         "image": "../assets/images/recommended/disney-100.png",
         "title": "LEGO® Minifigures Disney 100",
         "price": "3.99$",
-        "category": "Toys"
+        "category": "Toys",
+        "id":24,
+        "pieces":8,
     }
 
      
