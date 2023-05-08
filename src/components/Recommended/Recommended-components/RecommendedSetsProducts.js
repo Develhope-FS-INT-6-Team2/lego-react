2b456ba56a9c0c0a61b40ecb9ab7b0897db1d12e
AlbertoNeira <124807146+AlbertoNeira@users.noreply.github.com>
2023-05-08 13:36:08 +0200

    updated list product of recommended

--- src/components/Recommended/Recommended-components/RecommendedSetsProducts.js
+++ src/components/Recommended/Recommended-components/RecommendedSetsProducts.js
@@ -1,52 +1,84 @@
 let sets =[
     
-    {image:"../assets/images/featured-sets/titanic.png",
-     title:"Titanic",
-     price:"79.99$"},
-
-     {image:"../assets/images/featured-sets/van-gogh.png",
-     title:"Van Gogh",
-     price:"39.99$"},
-
-     {image:"../assets/images/featured-sets/hogwarts-room-of-requirement.png",
-     title:"Hogwarts Room of Requirements",
-     price:"59.99$"},
-
-     {image:"../assets/images/featured-sets/temple-of-the-golden-idol.png",
-     title:"Temple of The Golden Idol",
-     price:"59.99$"},
-
-     {image:"../assets/images/featured-sets/the-razor-crest.png",
-     title:"The Razor Crest",
-     price:"59.99$"},
-     
-    {image:"../assets/images/featured-sets/cat-hotel.png",
-     title:"Cat hotel",
-     price:"59.99$"},
-
-     {image:"../assets/images/featured-sets/jazz-club.png",
-     title:"Jazz-club",
-     price:"79.99$"},
-
-     {image:"../assets/images/featured-sets/golden-ultra-dragon.png",
-     title:"Golden Ultra Dragon",
-     price:"109.99$"},
-
-     {image:"../assets/images/featured-sets/grand-piano.png",
-     title:"Grand Piano",
-     price:"39.99$"},
-
-     {image:"../assets/images/featured-sets/star-lord-helmet.png",
-     title:"Star Lord Helmet",
-     price:"45.99$"},
-
-     {image:"../assets/images/featured-sets/tie-bomber.png",
-     title:"Tie Bomber",
-     price:"59.99$"},
-
-     {image:"../assets/images/featured-sets/eiffel-tower.png",
-     title:"Eiffel Tower",
-     price:"109.99$"},
+   
+    
+    {
+        "image": "../assets/images/recommended/emperor-throne.png",
+        "title": "Emperor's Throne Room™ Diorama",
+        "price": "89.99$",
+        "category": "Movies"
+    },
+    {
+        "image": "../assets/images/recommended/endor-speeder.png",
+        "title": "Endor™ Speeder Chase Diorama",
+        "price": "69.99$",
+        "category": "Movies"
+    },
+    {
+        "image": "../assets/images/recommended/mandalorian-fang-figther.png",
+        "title": "Mandalorian Fighter vs. TIE™",
+        "price": "89.99$",
+        "category": "Movies"
+    },
+    {
+        "image": "../assets/images/recommended/starfighter.png",
+        "title": "X-Wing Starfighter™",
+        "price": "209.99$",
+        "category": "Movies"
+    },
+    {
+        "image": "../assets/images/recommended/spider-tank.png",
+        "title": "Spider Tank",
+        "price": "39.99$",
+        "category": "Movies"
+    },
+    {
+        "image": "../assets/images/recommended/battle-endor-heros.png",
+        "title": "Battle of Endor™ Heroes",
+        "price": "39.99$",
+        "category": "Movies"
+    },
+ 
+    {
+        "image": "../assets/images/recommended/vader-keyring.png",
+        "title": "Darth Vader™ Keyring",
+        "price": "4.99$",
+        "category": "Keyring"
+    },
+  
+    {
+        "image": "../assets/images/recommended/mermaid.png",
+        "title": "The Little Mermaid Royal Clamshell",
+        "price": "139.99$",
+        "category": "toys"
+    },
+    {
+        "image": "../assets/images//recommended/peugeot.png",
+        "title": "PEUGEOT 9X8 Hybrid Hypercar",
+        "price": "7169.99$",
+        "category": "Car"
+    },
+    {
+        "image": "../assets/images/recommended/land-rover.png",
+        "title": "Land Rover Classic Defender 90",
+        "price": "209.99$",
+        "category": "Car"
+    },
+  
+  
+    {
+        "image": "../assets/images/recommended/tales-space.png",
+        "title": "Tales of the Space Age",
+        "price": "59.99$",
+        "category": "Space"
+    },
+  
+    {
+        "image": "../assets/images/recommended/disney-100.png",
+        "title": "LEGO® Minifigures Disney 100",
+        "price": "3.99$",
+        "category": "Toys"
+    }
 
      
 ]
