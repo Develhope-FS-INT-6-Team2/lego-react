fd7046251f2f02ffe3f868abc7498fbae27c2882
Emre <ct.edag@gmail.com>
2023-05-25 17:26:05 +0300

    hotfixMPSv2

--- src/components/Header/Header-components/Account.js
+++ src/components/Header/Header-components/Account.js
@@ -22,8 +22,7 @@ function Account() {
         <img src="../assets/icons/header/vip-icon.png" height="18" width="26" alt="Vip"/>
         <Link to="/vip">VIP</Link>
 
-      </div>
-    </div>
+      </div>    
   );
 }
 
