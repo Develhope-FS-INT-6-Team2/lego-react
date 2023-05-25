69703ad5ff5690b8d408b67720449c163ebcf8c1
Nurettin Demirhan <ndemirhan1907@gmail.com>
2023-05-25 15:22:48 +0300

    product-page data adjusted. added other components to the product page

--- src/components/page-components/ProductPage/ProductPage.js
+++ src/components/page-components/ProductPage/ProductPage.js
@@ -4,6 +4,11 @@ import "./ProductPage.css";
 import data from "../../FeaturedSets/featured-sets-components/Products.json"
 import Footer from '../../Footer/Footer';
 import ProductPageItem from '../../product-page-item/ProductPageItem';
+import Header from '../../Header/Header';
+import MainMenu from '../../main-menu/MainMenu';
+import Recommended from '../../Recommended/Recommended';
+import {CookiePermission} from "../../cookie-permission/CookiePermission"
+
 
 export default function ProductDetail() {
   let { id } = useParams();
@@ -13,9 +18,12 @@ export default function ProductDetail() {
   // console.log((foundItem));
   return (
     <div className='product-page'>
-      <h1>{id}</h1>
-      <ProductPageItem  image={foundItem.image} title={foundItem.title} price={foundItem.price} id={id} />
+      <Header />
+      <MainMenu />
       
+      <ProductPageItem  image={foundItem.image} title={foundItem.title} price={foundItem.price} id={id} />
+      <CookiePermission/>
+      <Recommended image={foundItem.image} title={foundItem.title} price={foundItem.price} id={id}/>
       <Footer/>
 
     </div>
