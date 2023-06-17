54fdc17e29b7b57205a23f4f810d7ae580d2090f
AlbertoNeira <124807146+AlbertoNeira@users.noreply.github.com>
2023-06-17 14:15:12 +0200

    login and register endpoints

--- src/Database/connection.js
+++ src/Database/connection.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
-const Order = require('../models/order');
-const Product = require('../models/product');
-const User = require('../models/user')
+
 mongoose.set('debug', true);
 
 
 const express = require('express');
 const cors = require('cors');
-
+const userRoutes = require('../Routes/userRoutes');
 
 const app = express();
 const PORT = 3010;
@@ -32,61 +30,18 @@ async function connectToDatabase() {
   }
 }
 
+app.use('/api/user', userRoutes);
 
-
-async function  getAllProducts(){
-    let resultArray = [];
-  
-    await Promise.all([Order.init(), Product.init(), User.init()])
-      .then(async () => {
-        console.log('Schema uploaded successfully');
-
-        const result = await Product.find({});
-  
-        await result.forEach((item)=>{ console.log(item); resultArray.push(item)});
-        
-        //process.exit(0);
-      })
-      .catch((error) => {
-        console.error('Error uploading schema', error);
-        process.exit(1);
-      });
-    return resultArray;
-    
-}
-
-async function  getProductById(pId){
-  let resultArray = [];
-
-  await Promise.all([Order.init(), Product.init(), User.init()])
-    .then(async () => {
-      console.log('Schema uploaded successfully');
-
-      const result = await Product.find({id:pId});
-
-      await result.forEach((item)=>{ console.log(item); resultArray.push(item)});
-      
-    })
-    .catch((error) => {
-      console.error('Error uploading schema', error);
-      process.exit(1);
-    });
-  return resultArray;
-  
-}
-
-
-app.get('/getAllProducts',async (req,res)=>{
-  console.log("Recieved getAllProducts");
-  let result = await getAllProducts();
-  res.json(result);
+app.listen(PORT, ()=>{
+  console.log("Server has started at port",PORT,"you can Go to link from here "+`http://localhost:${PORT}`);
+  connectToDatabase();
 })
 
-app.get('/getProductById',async (req,res)=>{
-  console.log("Recieved getProductById");
-  let result = await getProductById(req.body.id);
-  res.json(result);
-})
+
+
+
+
+
 
 //register //post
 //login    //post
@@ -97,64 +52,3 @@ app.get('/getProductById',async (req,res)=>{
 //addOrder
 //cancelOrder
 
-
-
-app.listen(PORT, ()=>{
-  console.log("Server has started at port",PORT,"you can Go to link from here "+`http://localhost:${PORT}`);
-  connectToDatabase();
-})
-
-
-/*
-connectToDatabase().then(() => {
-  Promise.all([Order.init(), Product.init(), User.init()])
-    .then(async () => {
-      console.log('Schema uploaded successfully');
-
-      await Product.insertMany(productsData).then(()=>{
-        console.log("Added successfully");
-      }).catch((error)=>{
-        console.log("Error");
-
-      });
-
-
-      const result = await Product.find({});
-
-      result.forEach((item)=>{ console.log(item)});
-
-      //process.exit(0);
-    })
-    .catch((error) => {
-      console.error('Error uploading schema', error);
-      process.exit(1);
-    });
-});
-*/
-/*
-connectToDatabase().then(async () => {
-  try {
-    await Product.init();
-    console.log('Product schema initialized');
-    
-
-    await Product.insertMany(productsData).then(()=>{
-      console.log("Added successfully");
-    }).catch((error)=>{
-      console.log("Error");
-
-    });
-
-    console.log('Products added successfully');
-    console.log('Insert result:', result);
-
-    const insertedProducts = await Product.find();
-    console.log('Inserted products:', insertedProducts);
-
-    process.exit(0);
-  } catch (error) {
-    console.error('Error adding products', error);
-    console.error(error.stack);
-    process.exit(1);
-  }
-});*/
