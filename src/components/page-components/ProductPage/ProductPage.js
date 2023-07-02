fe8cd289ca853f6688a58737b3f76c268788e9df
Nurettin Demirhan <ndemirhan1907@gmail.com>
2023-07-02 15:58:23 +0300

    product page item connected to backend

--- src/components/page-components/ProductPage/ProductPage.js
+++ src/components/page-components/ProductPage/ProductPage.js
@@ -1,7 +1,8 @@
 import { useParams, Link } from 'react-router-dom';
-import React, { useContext } from 'react';
+import React, { useContext,useEffect,useState } from 'react';
+import axios from "axios";
 import "./ProductPage.css";
-import data from "../../FeaturedSets/featured-sets-components/Products.json";
+
 import Footer from '../../Footer/Footer';
 import ProductPageItem from '../../product-page-item/ProductPageItem';
 import Header from '../../Header/Header';
@@ -12,8 +13,25 @@ import { LoginContext } from '../../context/LoginContext';
 
 export default function ProductPage() {
   let { id } = useParams();
-  let foundItem = data.sets.find(item => item.id === Number(id));
+  // let foundItem = data.sets.find(item => item.id === Number(id));
   const { isLoggedIn } = useContext(LoginContext);
+
+  let [foundItem,setFoundItem] = useState([{id:"",image:"",price:"",title:""}])
+    
+  useEffect(() => {
+      fetchData();
+      
+    }, []);
+  
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3010/api/product/${id}`);
+        setFoundItem(response.data);
+        console.log(foundItem)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
   return (
     <div className='product-page'>
@@ -21,7 +39,7 @@ export default function ProductPage() {
       <MainMenu />
       <ProductPageItem image={foundItem.image} title={foundItem.title} price={foundItem.price} id={id} />
       <CookiePermission />
-      <Recommended image={foundItem.image} title={foundItem.title} price={foundItem.price} id={id}/>
+      <Recommended />
       <Footer />
     </div>
   );
