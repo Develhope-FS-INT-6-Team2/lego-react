2a5ea246930cdf29b312213eee9281f7633fb5ce
Roq4 <ocakkoc@gmail.com>
2023-07-06 01:51:27 +0300

    order endpoints create order & delete order

--- src/Database/connection.js
+++ src/Database/connection.js
@@ -1,49 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
-const bodyParser = require('body-parser');
-const express = require('express');
-const cors = require('cors');
-const userRoutes = require('../Routes/userRoutes');
-const productRoutes = require('../Routes/productRoutes');
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("../Routes/userRoutes");
+const productRoutes = require("../Routes/productRoutes");
+const orderRoutes = require("../Routes/orderRoutes");
 
 const app = express();
 const PORT = 3010;
 
-
-
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
 
-
 async function connectToDatabase() {
   try {
-      await mongoose.connect('mongodb+srv://tester:1234@lego-db.lssxr4t.mongodb.net/?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferCommands: false, // Disable the buffering of commands
-      
-    });
-    console.log('Connected to MongoDB');
+    await mongoose.connect(
+      "mongodb+srv://tester:1234@lego-db.lssxr4t.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        bufferCommands: false, // Disable the buffering of commands
+      }
+    );
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB', error);
+    console.error("Error connecting to MongoDB", error);
   }
 }
 
-app.use('/api/user', userRoutes);
-app.use('/api/product', productRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/order", orderRoutes);
 
-app.listen(PORT, ()=>{
-  console.log("Server has started at port",PORT,"you can Go to link from here "+`http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(
+    "Server has started at port",
+    PORT,
+    "you can Go to link from here " + `http://localhost:${PORT}`
+  );
   connectToDatabase();
-})
-
-
-
-
-
-
+});
 
 //register //post
 //login    //post
--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,57 +1,56 @@
 //addOrder
 //cancelOrder
 
-const User = require('../models/user');
+const User = require("../models/user");
 
-const Order = require('../models/order');
+const Order = require("../models/order");
 
 // Add an order
 async function addOrder(req, res) {
   try {
-    // Extract order data from request body
-    const { orderId, userId, products, productCounts, date } = req.body;
+    const { userID, products, productCounts } = req.body;
 
-    // Create a new order in the database
-    const newOrder = await Order.create({ orderId, userId, products, productCounts, date });
+    if (!userID) {
+      return res
+        .status(400)
+        .json({ message: "Missing userID in the request body" });
+    }
 
-    console.log('Order added successfully:', newOrder);
+    // Create a new order instance
+    const order = new Order({
+      userID,
+      products,
+      productCounts,
+    });
 
-    // Find the user in the database and update the latestOrders array
-    const user = await User.findOne({ _id: userId });
-    user.latestOrders.push(newOrder._id);
-    await user.save();
+    // Save the order to the database
+    await order.save();
 
-    res.json({ message: 'Order added successfully' });
+    res.status(201).json({ message: "Order created successfully", order });
   } catch (error) {
-    console.error('Error adding order', error);
-    res.status(500).json({ error: 'Failed to add order' });
+    res
+      .status(500)
+      .json({ message: "Failed to create order", error: error.message });
   }
 }
 
 // Cancel an order
 async function cancelOrder(req, res) {
   try {
-    // Extract orderId from request parameters
     const { orderId } = req.params;
 
-    // Find the order in the database and remove it
-    const deletedOrder = await Order.findOneAndDelete({ orderId });
+    // Find the order by ID in the database and remove it
+    const order = await Order.findByIdAndRemove(orderId);
 
-    if (!deletedOrder) {
-      return res.status(404).json({ error: 'Order not found' });
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
     }
 
-    console.log('Order canceled successfully:', deletedOrder);
-
-    // Find the user in the database and remove the order ID from the latestOrders array
-    const user = await User.findOne({ _id: deletedOrder.userId });
-    user.latestOrders = user.latestOrders.filter(id => id.toString() !== deletedOrder._id.toString());
-    await user.save();
-
-    res.json({ message: 'Order canceled successfully' });
+    res.status(200).json({ message: "Order deleted successfully" });
   } catch (error) {
-    console.error('Error canceling order', error);
-    res.status(500).json({ error: 'Failed to cancel order' });
+    res
+      .status(500)
+      .json({ message: "Failed to delete order", error: error.message });
   }
 }
 
